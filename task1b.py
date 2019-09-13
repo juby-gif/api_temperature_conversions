dc3f771126db87ffbadc354c6dafbad546f7795e
JUBY VARUGHESE <juby@bcinnovationlabs.com>
2019-09-12 23:23:38 -0400

    this is my edit

--- task1b.py
+++ task1b.py
@@ -1,8 +1,8 @@
 def c_to_k(c):
-    k = c + 273.15 #Formula to convert Celcius to Kelvin
+    k = c + 273.15 #Formula to convert Celsius to Kelvin
     return k #Return the value of k to where it stopped(line 10)
 def f_to_c(f):
-    fa = (f-32) * 5/9 #Formula to convert Fareheit to Celcius
+    fa = (f-32) * 5/9 #Formula to convert Fareheit to Celsius
     return fa #Return the value of fa to where it stopped(line 11)
 c = 25.0
 f = 100.0
@@ -10,5 +10,5 @@ f = 100.0
 k = c_to_k(c)
 fa = f_to_c(f)
 
-print("Celcius of " + str(c) + "  is " + str(k) + " in Kelvin")
-print("Farenheit of " + str(f) + "  is " + str(fa) + " in Celcius")
+print("Celsius of " + str(c) + "  is " + str(k) + " in Kelvin")
+print("Farenheit of " + str(f) + "  is " + str(fa) + " in Celsius")
